7709e7c1e50e9ca9715d71df702894dda67db390
PavelKovalchuk <kovpavanat@gmail.com>
2018-11-11 18:16:37 +0200

    multiple entry points in Root Query

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -37,14 +37,22 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-      user: {
-          type: UserType,
-          args: {id: {type: GraphQLString}},
-          resolve(parentValue, args) {
-              return axios.get(`http://localhost:3000/users/${args.id}`)
-                  .then(resp => resp.data);
-          },
-      }
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
+            },
+        },
+        company: {
+            type: CompanyType,
+            args: {id: {type: GraphQLString}},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                    .then(resp => resp.data);
+            },
+        }
     },
 });
 
