9189c785a68a8077c4c6dd3e5facebd078e40685
PavelKovalchuk <kovpavanat@gmail.com>
2018-11-18 19:31:55 +0200

    added router and basic form

--- app/client/components/SongList.js
+++ app/client/components/SongList.js
@@ -4,21 +4,39 @@ import {graphql} from 'react-apollo';
 
 class SongList extends Component {
 
-    render() {
-        console.log('props', this.props);
-        return (
-            <div>
-                SongList
-            </div>
-        );
+  renderSongs() {
+    return this.props.data.songs.map((song => {
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
+    }));
+  }
+
+  render() {
+
+    if (this.props.data.loading) {
+      return (
+        <div>
+          Loading
+        </div>
+      );
     }
+    return (
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
+    );
+  }
 }
 
 const query = gql`
 {
-   songs {    
-      title    
-   }
+  songs { 
+    id   
+    title    
+  } 
 }    
 `;
 
