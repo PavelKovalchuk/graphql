d98e26dbbec207bc8169fefcaaf261af6e101439
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-01 18:59:45 +0200

    auth login and logout schemas mutation

--- auth/server/schema/mutations.js
+++ auth/server/schema/mutations.js
@@ -4,6 +4,7 @@ const {
   GraphQLString,
 } = grapql;
 const UserType = require('./types/user_type');
+const AuthService = require('../services/auth');
 
 const mutation = new GraphQLObjectType({
   name: 'Mutation',
@@ -15,7 +16,29 @@ const mutation = new GraphQLObjectType({
         password: {type: GraphQLString},
       },
       resolve(parentValue, args, request) {
-
+        return AuthService.signup({
+          email: args.email,
+          password: args.password,
+          req: request,
+        });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, request) {
+        const {user} = request;
+        request.logout();
+        return user;
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: {type: GraphQLString},
+        password: {type: GraphQLString},
+      },
+      resolve(parentValue, {email, password}, request) {
+        return AuthService.login({email, password, req: request});
       }
     }
   },
