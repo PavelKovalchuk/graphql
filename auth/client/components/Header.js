a014c425e8a930c72ffce084645ae863cce220a4
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-02 15:15:04 +0200

    auth client - logout functionality

--- auth/client/components/Header.js
+++ auth/client/components/Header.js
@@ -1,17 +1,59 @@
 import React, {Component} from 'react';
 import {graphql} from 'react-apollo';
 import query from '../queries/CurrentUser';
+import {Link} from 'react-router';
+import mutation from '../mutations/Logout';
 
 class Header extends Component{
+
+  onLogoutClick() {
+    // Call a mutation
+    this.props.mutate({
+      refetchQueries: [{query}],
+    });
+  }
+
+  renderButtons() {
+    const {loading, user} = this.props.data;
+    if (loading) {
+      return <div>Loading</div>
+    }
+
+    if (user) {
+      return (
+        <li>
+          <a onClick={this.onLogoutClick.bind(this)} >Logout</a>
+        </li>
+      );
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/login">Log in</Link>
+          </li>
+        </div>
+      );
+    }
+  }
 
   render() {
     console.log(this.props);
     return (
-      <div>
-        Header
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">Home</Link>
+          <ul className="right">
+            {this.renderButtons()}
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
 
-export default graphql(query)(Header);
+export default graphql(mutation)(
+  graphql(query)(Header)
+);
