9090107a57ab0aa8ca5c5b8bc07d28ebc45f8202
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-21 16:27:50 +0200

    Routing

--- tutorial/news/src/index.js
+++ tutorial/news/src/index.js
@@ -7,6 +7,7 @@ import {ApolloProvider} from "react-apollo";
 import {ApolloClient} from "apollo-client";
 import {createHttpLink} from "apollo-link-http";
 import {InMemoryCache} from "apollo-cache-inmemory";
+import {BrowserRouter} from 'react-router-dom'
 
 /**
  * will connect your ApolloClient instance with the GraphQL API,
@@ -23,9 +24,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
