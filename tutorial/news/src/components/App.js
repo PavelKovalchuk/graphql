f1f7ff1508cc4dd200a5f53df0dd2c96414bf62c
PavelKovalchuk <kovpavanat@gmail.com>
2018-12-21 13:52:10 +0200

    added prettier

--- tutorial/news/src/components/App.js
+++ tutorial/news/src/components/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import LinkList from './LinkList'
-import '../styles/App.css';
+import React, {Component} from "react";
+import LinkList from "./LinkList";
+import "../styles/App.css";
 
 class App extends Component {
   render() {
-    return <LinkList />
+    return <LinkList />;
   }
 }
 
--- tutorial/news/src/index.js
+++ tutorial/news/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import {ApolloProvider} from "react-apollo";
+import {ApolloClient} from "apollo-client";
+import {createHttpLink} from "apollo-link-http";
+import {InMemoryCache} from "apollo-cache-inmemory";
 
 /**
  * will connect your ApolloClient instance with the GraphQL API,
@@ -14,19 +14,19 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
  * @type {ApolloLink}
  */
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
+  uri: "http://localhost:4000",
 });
 
 const client = new ApolloClient({
   link: httpLink,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
